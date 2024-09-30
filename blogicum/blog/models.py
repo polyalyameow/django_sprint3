43da0fd6eae7a0775af2906595389471141b741c
polyalyameow <117765092+polyalyameow@users.noreply.github.com>
2024-09-30 22:27:23 +0200

    Changes based on review

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+from .constants import CHAR_FIELD_MAX_LENGTH
+
 User = get_user_model()
 
 
-class BaseModel(models.Model):
+class Timestamps(models.Model):
     created_at = models.DateTimeField(auto_now_add=True,
                                       verbose_name='Добавлено')
     is_published = models.BooleanField(default=True,
@@ -16,29 +18,38 @@ class BaseModel(models.Model):
         abstract = True
 
 
-class Category(BaseModel):
-    title = models.CharField(max_length=256, verbose_name='Заголовок')
+class Category(Timestamps):
+    title = models.CharField(max_length=CHAR_FIELD_MAX_LENGTH,
+                             verbose_name='Заголовок')
     description = models.TextField(verbose_name='Описание')
     slug = models.SlugField(unique=True, verbose_name='Идентификатор',
                             help_text=('Идентификатор страницы для URL; '
                                        'разрешены символы латиницы, цифры, '
                                        'дефис и подчёркивание.'))
 
+    def __str__(self):
+        return self.title
+
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'Категории'
 
 
-class Location(BaseModel):
-    name = models.CharField(max_length=256, verbose_name='Название места')
+class Location(Timestamps):
+    name = models.CharField(max_length=CHAR_FIELD_MAX_LENGTH,
+                            verbose_name='Название места')
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         verbose_name = 'местоположение'
         verbose_name_plural = 'Местоположения'
 
 
-class Post(BaseModel):
-    title = models.CharField(max_length=256, verbose_name='Заголовок')
+class Post(Timestamps):
+    title = models.CharField(max_length=CHAR_FIELD_MAX_LENGTH,
+                             verbose_name='Заголовок')
     text = models.TextField(verbose_name='Текст')
     pub_date = models.DateTimeField(auto_now=False, auto_now_add=False,
                                     verbose_name='Дата и время публикации',
@@ -47,13 +58,19 @@ class Post(BaseModel):
                                                'делать отложенные '
                                                'публикации.'))
     author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               verbose_name='Автор публикации')
+                               verbose_name='Автор публикации',
+                               related_name='posts')
     location = models.ForeignKey(Location, on_delete=models.SET_NULL,
                                  null=True,
-                                 verbose_name='Местоположение')
+                                 verbose_name='Местоположение',
+                                 related_name='posts')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL,
                                  null=True,
-                                 verbose_name='Категория')
+                                 verbose_name='Категория',
+                                 related_name='posts')
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         verbose_name = 'публикация'
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,17 +1,17 @@
-from django.db.models import Q
 from django.utils import timezone
 
 from django.shortcuts import get_object_or_404, render
 from blog.models import Post, Category
+from .constants import NUM_LATEST_POSTS
 
 now = timezone.now()
 
 
 def index(request):
     template = 'blog/index.html'
-    posts = Post.objects.filter(Q(pub_date__lt=now) & Q(is_published=True)
-                                & Q(category__is_published=True)
-                                ).order_by('-pub_date')[:5]
+    posts = Post.objects.filter(pub_date__lt=now, is_published=True,
+                                category__is_published=True
+                                ).order_by('-pub_date')[:NUM_LATEST_POSTS]
     context = {'post_list': posts}
     return render(request, template, context)
 
@@ -30,7 +30,7 @@ def category_posts(request, category_slug):
 
     category = get_object_or_404(Category, slug=category_slug,
                                  is_published=True)
-    post_list = category.post_set.filter(is_published=True, pub_date__lte=now)
+    post_list = category.posts.filter(is_published=True, pub_date__lte=now)
     context = {'category': category,
                'post_list': post_list}
     return render(request, template, context)
